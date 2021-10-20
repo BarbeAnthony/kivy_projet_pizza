fd6b444a3380d0d65109e7d2d77f0d1b2a4fbfc4
AnthonyBarbe <barbe.anthony33@gmail.com>
2021-10-20 19:10:36 +0200

    Ajout de données pizzas en dur

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from models import Pizza
 
 
 class MainWidget(BoxLayout):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.pizzas=[
+            Pizza("4 fromages", "chèvre, comté, emmental, brie", 9.5, True),
+            Pizza("Chorizo", "tomates, chorizo, parmesan", 11.2, False),
+            Pizza("Calzone", "fromage, jambon, champignons", 10, False),
+        ]
+
 
 class PizzaApp(App):
     pass
