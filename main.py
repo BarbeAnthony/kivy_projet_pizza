86419323e1b625c6351c4c86180fdc6aada07a39
AnthonyBarbe <barbe.anthony33@gmail.com>
2021-10-21 02:11:35 +0200

    Affichage des données dans RecycleView

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+import operator
 from kivy.app import App
+from kivy.properties import ObjectProperty, StringProperty, NumericProperty, BooleanProperty
 from kivy.uix.boxlayout import BoxLayout
 from models import Pizza
 
 
+class PizzaWidget(BoxLayout):
+    nom = StringProperty("")
+    ingredients = StringProperty("")
+    prix = NumericProperty(0.0)
+    vegetarienne = BooleanProperty(False)
+
+
 class MainWidget(BoxLayout):
+    proprieteRecycleView = ObjectProperty(None)
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.pizzas=[
@@ -12,8 +23,24 @@ class MainWidget(BoxLayout):
             Pizza("Calzone", "fromage, jambon, champignons", 10, False),
         ]
 
+    def construire_dictionnaire(self, pizza):
+        return {
+            "nom": pizza.nom,
+            "ingredients": pizza.ingredients,
+            "prix": pizza.prix,
+            "vegetarienne": pizza.vegetarienne
+        }
+
+    def on_parent(self, widget, parent):
+        liste_pizzas = []
+        for pizza in self.pizzas:
+            liste_pizzas.append(self.construire_dictionnaire(pizza))
+        liste_pizzas.sort(key=operator.itemgetter("prix"))
+        self.proprieteRecycleView.data = liste_pizzas
+
 
 class PizzaApp(App):
     pass
+
 
 PizzaApp().run()
